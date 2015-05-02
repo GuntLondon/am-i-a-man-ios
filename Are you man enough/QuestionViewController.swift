d463a6db677ab6435f89baac73bd2af5664f4278
Remi <remi.shergold@hotmail.com>
2015-05-02 17:38:53 +0100

    question view controller layout

--- Are you man enough/QuestionViewController.swift
+++ Are you man enough/QuestionViewController.swift
@@ -2,41 +2,37 @@
 //  QuestionViewController.swift
 //  Are you man enough
 //
-//  Created by Hatch on 04/02/2015.
-//  Copyright (c) 2015 Hatch. All rights reserved.
-//
+
 
 import UIKit
 
-class QuestionViewController: BaseController {
+class QuestionViewController: UIViewController {
     
-    @IBOutlet var progress_label: UILabel!
-    @IBOutlet var title_label: UILabel!
-    @IBOutlet var question_label: UILabel!
-    
+
+  
+  @IBOutlet weak var background: UIImageView!
+  @IBOutlet weak var man: UIImageView!
+  
+  @IBOutlet weak var speechBubble: UIView!
+  
+  @IBOutlet weak var questionProgress: UILabel!
+  @IBOutlet weak var question: UILabel!
+  
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        progress_label.text = "\(quiz.question.index+1)/\(quiz.category.questions.count)"
-        title_label.text = quiz.category.title
-        question_label.text = quiz.question.content
+
         
     }
 
     @IBAction func yes_button(sender: AnyObject) {
-        quiz.answer(true)
-        celebration()
+
     }
     
     @IBAction func no_button(sender: AnyObject) {
-        quiz.answer(false)
-        go_to_next_screen()
+
     }
     
-    // TODO: can we use an emoj for function name?
-    func celebration() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewControllerWithIdentifier("celebration_view") as UIViewController
-        self.showViewController(vc, sender: self)
-    }
 }
